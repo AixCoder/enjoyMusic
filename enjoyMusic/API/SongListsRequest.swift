ea678d7c0e8ac5cda94f1692ade0e367c4e413b3
liuhongnian <liuhongnian16@gmail.com>
2017-12-23 20:00:15 +0800

    应用第一版完成 简单播放晚安歌曲

---
+++ enjoyMusic/API/SongListsRequest.swift
@@ -0,0 +1,13 @@
+//
+//  SongListsRequest.swift
+//  enjoyMusic
+//
+//  Created by liuhongnian on 7/29/17.
+//  Copyright © 2017 liuhongnian. All rights reserved.
+//
+
+import UIKit
+
+class SongListsRequest: YTKRequest {
+
+}
